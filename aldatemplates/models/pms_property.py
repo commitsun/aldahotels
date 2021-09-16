b800798b1ab9ccbceaa0f78e49cf7b89ef29c4cc
braisab <braisterbutalino@gmail.com>
2021-09-16 21:11:34 +0200

    [IMP] changes in pms_property model and view

--- aldatemplates/models/pms_property.py
+++ aldatemplates/models/pms_property.py
@@ -4,21 +4,32 @@ from odoo import fields, models
 class PmsProperty(models.Model):
     _inherit = "pms.property"
 
-    confirmation_template_id = fields.Many2one(
+    property_confirmed_template = fields.Many2one(
         string="Confirmation Template",
         comodel_name="mail.template",
-        # default=lambda self: self._get_default_confirmation_template_id(),
+        default=lambda self: self.env.ref(
+            "aldatemplates.alda_confirmation_email", raise_if_not_found=False
+        ),
     )
-    image1 = fields.Binary(string="Image City", attachment=True, store=True)
-    image2 = fields.Binary(string="Image Property", attachment=True, store=True)
+    property_exit_template = fields.Many2one(
+        string="Exit Template",
+        comodel_name="mail.template",
+        default=lambda self: self.env.ref(
+            "aldatemplates.alda_exit_email", raise_if_not_found=False
+        ),
+    )
+    property_canceled_template = fields.Many2one(
+        string="Cancellation Template",
+        comodel_name="mail.template",
+        default=lambda self: self.env.ref(
+            "aldatemplates.alda_cancelation_email", raise_if_not_found=False
+        ),
+    )
+    image1 = fields.Char(string="Image City", attachment=True, store=True)
+    image2 = fields.Char(string="Image Property", attachment=True, store=True)
     text_image1 = fields.Html(
         string="Text Image City",
     )
     text_image2 = fields.Html(string="Text Image Property")
     web_city_information_url = fields.Char(string="Web City Info URL")
-    survey_url = fields.Char(string="Survey URL")
-
-    #
-    # def _get_default_confirmation_template_id(self):
-    #     default_template_id = self.env.ref("aldatemplates.alda_confirmation_email").id
-    #     return default_template_id
+    quiz_url = fields.Char(string="Quiz URL")
