8a084a4ed31a369b3ef8ac1770f993d2c6ebafd2
Darío Lodeiros <dario@commitsun.com>
2021-09-29 10:04:45 +0200

    [IMP]migration: add migration log pricelist

--- migrated_hotel/models/migrated_log.py
+++ migrated_hotel/models/migrated_log.py
@@ -1,28 +1,33 @@
 # Copyright 2019  Pablo Q. Barriuso
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class MigrateLog(models.Model):
-    _name = 'migrated.log'
+    _name = "migrated.log"
 
-    name = fields.Char('Message')
+    name = fields.Char("Message")
     date_time = fields.Datetime()
-    migrated_hotel_id = fields.Many2one('migrated.hotel')
-    model = fields.Selection([
-        ('partner', 'res.partner'),
-        ('product', 'product.product'),
-        ('folio', 'hotel.folio'),
-        ('reservation', 'hotel.reservation'),
-        ('service', 'hotel.service'),
-        ('payment', 'account.payment'),
-        ('return', 'payment.return'),
-        ('invoice', 'account.invoice'),
-        ('journal', 'account.journal'),
-    ])
+    migrated_hotel_id = fields.Many2one("migrated.hotel")
+    model = fields.Selection(
+        [
+            ("partner", "res.partner"),
+            ("product", "product.product"),
+            ("folio", "hotel.folio"),
+            ("reservation", "hotel.reservation"),
+            ("service", "hotel.service"),
+            ("payment", "account.payment"),
+            ("return", "payment.return"),
+            ("invoice", "account.invoice"),
+            ("journal", "account.journal"),
+            ("pricelist", "product.pricelist"),
+        ]
+    )
     remote_id = fields.Integer(
-        copy=False, readonly=True,
-        help="ID of the remote record in the previous version")
+        copy=False,
+        readonly=True,
+        help="ID of the remote record in the previous version",
+    )
 
-    _order = 'date_time desc'
+    _order = "date_time desc"
