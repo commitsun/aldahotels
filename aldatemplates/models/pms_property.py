c61f048a51f94084fbb8ced5b03187c535e5a7fc
braisab <38132068+braisab@users.noreply.github.com>
2021-10-20 11:56:18 +0200

    [WIP] added new module for alda templates (#1)
    * [ADD] added new module for alda templates
    
    * [IMP]: added fields in pms_property for configuration email template
    
    * [IMP] changes in email template
    
    * [REF] refactor confirmation email template
    
    * [IMP] added exit and cancelation email templates
    
    * [IMP] added pricelist_email_text field in product_pricelist
    
    * [IMP] changes in pms_property model and view
    
    * [IMP] changed model_id field of templates, from pms_reservation to pms_folio

---
+++ aldatemplates/models/pms_property.py
@@ -0,0 +1,35 @@
+from odoo import fields, models
+
+
+class PmsProperty(models.Model):
+    _inherit = "pms.property"
+
+    property_confirmed_template = fields.Many2one(
+        string="Confirmation Template",
+        comodel_name="mail.template",
+        default=lambda self: self.env.ref(
+            "aldatemplates.alda_confirmation_email", raise_if_not_found=False
+        ),
+    )
+    property_exit_template = fields.Many2one(
+        string="Exit Template",
+        comodel_name="mail.template",
+        default=lambda self: self.env.ref(
+            "aldatemplates.alda_exit_email", raise_if_not_found=False
+        ),
+    )
+    property_canceled_template = fields.Many2one(
+        string="Cancellation Template",
+        comodel_name="mail.template",
+        default=lambda self: self.env.ref(
+            "aldatemplates.alda_cancelation_email", raise_if_not_found=False
+        ),
+    )
+    image1 = fields.Char(string="Image City", attachment=True, store=True)
+    image2 = fields.Char(string="Image Property", attachment=True, store=True)
+    text_image1 = fields.Html(
+        string="Text Image City",
+    )
+    text_image2 = fields.Html(string="Text Image Property")
+    web_city_information_url = fields.Char(string="Web City Info URL")
+    quiz_url = fields.Char(string="Quiz URL")
