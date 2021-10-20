69ca6f5d17408e585304c53d467e79acc6d30877
braisab <braisterbutalino@gmail.com>
2021-10-20 23:13:38 +0200

    [IMP]aldatemplates: added precheckin link in confirmation template & improvements in styles

--- aldatemplates/models/product_pricelist.py
+++ aldatemplates/models/product_pricelist.py
@@ -1,15 +1,7 @@
-from odoo import fields, models, api
+from odoo import fields, models
 
 
 class ProductPricelist(models.Model):
-    """Before creating a 'daily' pricelist, you need to consider the following:
-    A pricelist marked as daily is used as a daily rate plan for room types and
-    therefore is related only with one property.
-    """
-
     _inherit = "product.pricelist"
 
-
-    pricelist_email_text = fields.Html(
-        string="Pricelist Email Text"
-    )
+    pricelist_email_text = fields.Html(string="Pricelist Email Text")
