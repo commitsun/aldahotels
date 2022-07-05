f3f8923683c01bd012ffa155477f6eb6d27c6f3f
Jose Luis Algara <osotranquilo@gmail.com>
2022-07-05 11:15:16 +0200

    [ADD] data_bi_ref Field

--- pms_data_bi/__manifest__.py
+++ pms_data_bi/__manifest__.py
@@ -1,20 +1,21 @@
-# Copyright 2018-2021 Jose Luis Algara Toledo
+# Copyright 2018-2022 Jose Luis Algara Toledo
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "PMS Data Bi",
     "summary": "Export hotel data for business intelligence",
-    "version": "14.0.3.0.0",
+    "version": "14.0.3.1.0",
     "license": "AGPL-3",
     "author": "Jose Luis Algara (Alda hotels) <osotranquilo@gmail.com>, "
     "Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/pms",
+    "website": "https://github.com/OsoTranquilo/aldahotels.git",
     "depends": ["pms", "pms_l10n_es"],
     "category": "Generic Modules/Property Management System",
     "data": [
         "views/budget.xml",
         "views/inherit_pms_property.xml",
         "views/inherit_res_users.xml",
+        "views/inherit_res_partners.xml",
         "security/data_bi.xml",
         "security/ir.model.access.csv",
     ],
