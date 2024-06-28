c7ffa144f37ded2ee133670fe61a0842187b10f5
irluidev <iramirez.spain@gmail.com>
2024-06-28 19:26:38 +0200

    Fix comments and help

--- pms_information/models/pms_property.py
+++ pms_information/models/pms_property.py
@@ -11,74 +11,71 @@ class PmsProperty(models.Model):
 
     total_rooms = fields.Integer(
         string='Total rooms', 
-        compute='_compute_total_rooms'
+        compute='_compute_total_rooms',
+        help="Number of rooms in the hotel.",
     )
 
     company_vat = fields.Char(
         string='NIF', 
-        compute='_compute_company_vat'
+        compute='_compute_company_vat',
+        help="TAX ID number of the company to which the hotel belongs.",
     )
 
     email= fields.Char(
         string='Email Hotel', 
-        compute='_compute_email'
+        compute='_compute_email',
+        help="Email direction of the property.",
     )
 
     bank= fields.Char(
         string='Bank', 
-        compute='_compute_swift'
+        compute='_compute_swift',
+        help="Name of Bank of the company to wich the hotel belongs",
     )
 
     acc_number= fields.Char(
         string='Account Bank', 
-        compute='_compute_swift'
+        compute='_compute_swift',
+        help="Bank account number of the company to wich hotel belongs.",
     )
     swift= fields.Char(
         string='Swift', 
-        compute='_compute_swift'
+        compute='_compute_swift',
+        help="Bank Swift number of the company to wich hotel belongs.",
     )
 
     total_tourism_rooms = fields.Integer(
         string='Tourism Rooms',
+        help="Number of rooms in the hotel.",
+        default= 0
+        
     )
 
     open_date = fields.Datetime(
         string='Open date',
         default=fields.Datetime.now,
+        help="Property opening date."
     )
 
     is_ramp_up = fields.Boolean(
         string='Ramp-Up', 
         compute='_compute_state', 
         default=True,
+        help="From the opening of a hotel the first 17 months a hotel has RAMP-UP status, from month 17 it has RAMP-RATE status.",
     )
     
     is_ramp_rate = fields.Boolean(
         string='Ramp-Rate', 
         compute='_compute_state',
         default=False,
+        help="From the opening of a hotel the first 17 months a hotel has RAMP-UP status, from month 17 it has RAMP-RATE status.",
     )
 
     months_to_ramp_rate = fields.Integer(
         string='Month to Ramp-Rate', 
-        compute='_compute_months_to_ramp_rate'
+        compute='_compute_months_to_ramp_rate',
+        help="From the opening of a hotel the first 17 months a hotel has RAMP-UP status, from month 17 it has RAMP-RATE status.",
     )
-
-    months_open = fields.Integer(
-        string='Months Open',
-        compute='_compute_months_open',
-    )
-
-    @api.depends('open_date')
-    def _compute_months_open(self):
-        for record in self:
-            if record.open_date:
-                open_date = fields.Datetime.from_string(record.open_date)
-                today = datetime.now()
-                diff_months = (today.year - open_date.year) * 12 + today.month - open_date.month
-                record.months_open = diff_months
-            else:
-                record.months_open = 0
 
     @api.depends('open_date')
     def _compute_total_rooms(self):
