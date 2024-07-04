8040b552b6a1533759783b3e6f1199034c339592
irluidev <iramirez.spain@gmail.com>
2024-07-04 14:29:45 +0200

    ADD ALDA template mail - Purchase Order

--- purchase_portal/__manifest__.py
+++ purchase_portal/__manifest__.py
@@ -52,6 +52,7 @@
         'wizard/purchase_request_line_make_purchase_order.xml',
         'wizard/stock_backorder_confirmation_views.xml',
         'wizard/import_supplier_data_wizard.xml',
+        'data/purchase_pos_send_template.xml'
     ],
     "assets": {
         "web.assets_frontend": [],
